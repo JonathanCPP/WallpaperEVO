9655c6bd1c32727146ec32984f6a9121cdb1792a
JonathanCPP <jonathandale56@gmail.com>
2021-09-06 10:53:51 +0200

    Add usefull buttons

--- WallpaperEVO/WallpaperEVO/frmMain.Designer.cs
+++ WallpaperEVO/WallpaperEVO/frmMain.Designer.cs
@@ -34,17 +34,24 @@ namespace WallpaperEVO
             this.btnMinimize = new System.Windows.Forms.Button();
             this.pnlSearch = new System.Windows.Forms.Panel();
             this.pnlContent = new System.Windows.Forms.Panel();
+            this.txbSearch = new System.Windows.Forms.TextBox();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblLogo = new System.Windows.Forms.Label();
             this.pnlToolbar.SuspendLayout();
+            this.pnlSearch.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlToolbar
             // 
             this.pnlToolbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pnlToolbar.Controls.Add(this.lblLogo);
             this.pnlToolbar.Controls.Add(this.btnMinimize);
             this.pnlToolbar.Controls.Add(this.btnQuit);
-            this.pnlToolbar.Location = new System.Drawing.Point(0, 0);
+            this.pnlToolbar.Location = new System.Drawing.Point(0, -3);
             this.pnlToolbar.Name = "pnlToolbar";
-            this.pnlToolbar.Size = new System.Drawing.Size(1012, 40);
+            this.pnlToolbar.Size = new System.Drawing.Size(1023, 43);
             this.pnlToolbar.TabIndex = 0;
             this.pnlToolbar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DragMove);
             // 
@@ -55,11 +62,11 @@ namespace WallpaperEVO
             this.btnQuit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.btnQuit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.btnQuit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnQuit.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuit.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnQuit.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnQuit.Location = new System.Drawing.Point(956, 5);
+            this.btnQuit.Location = new System.Drawing.Point(964, 9);
             this.btnQuit.Name = "btnQuit";
-            this.btnQuit.Size = new System.Drawing.Size(45, 30);
+            this.btnQuit.Size = new System.Drawing.Size(45, 25);
             this.btnQuit.TabIndex = 1;
             this.btnQuit.Text = "X";
             this.btnQuit.UseVisualStyleBackColor = false;
@@ -72,11 +79,11 @@ namespace WallpaperEVO
             this.btnMinimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.btnMinimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
             this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMinimize.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimize.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMinimize.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnMinimize.Location = new System.Drawing.Point(905, 5);
+            this.btnMinimize.Location = new System.Drawing.Point(913, 9);
             this.btnMinimize.Name = "btnMinimize";
-            this.btnMinimize.Size = new System.Drawing.Size(45, 30);
+            this.btnMinimize.Size = new System.Drawing.Size(45, 25);
             this.btnMinimize.TabIndex = 2;
             this.btnMinimize.Text = "-";
             this.btnMinimize.UseVisualStyleBackColor = false;
@@ -86,6 +93,10 @@ namespace WallpaperEVO
             // 
             this.pnlSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
             this.pnlSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSearch.Controls.Add(this.btnSearch);
+            this.pnlSearch.Controls.Add(this.btnInfo);
+            this.pnlSearch.Controls.Add(this.btnSettings);
+            this.pnlSearch.Controls.Add(this.txbSearch);
             this.pnlSearch.Location = new System.Drawing.Point(12, 46);
             this.pnlSearch.Name = "pnlSearch";
             this.pnlSearch.Size = new System.Drawing.Size(989, 92);
@@ -100,6 +111,70 @@ namespace WallpaperEVO
             this.pnlContent.Size = new System.Drawing.Size(989, 476);
             this.pnlContent.TabIndex = 2;
             // 
+            // txbSearch
+            // 
+            this.txbSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.txbSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSearch.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txbSearch.Location = new System.Drawing.Point(271, 28);
+            this.txbSearch.MaxLength = 100;
+            this.txbSearch.Name = "txbSearch";
+            this.txbSearch.Size = new System.Drawing.Size(476, 29);
+            this.txbSearch.TabIndex = 0;
+            this.txbSearch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.BackgroundImage = global::WallpaperEVO.Properties.Resources.info3;
+            this.btnInfo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnInfo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInfo.FlatAppearance.BorderSize = 0;
+            this.btnInfo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
+            this.btnInfo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
+            this.btnInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInfo.Location = new System.Drawing.Point(224, 24);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(35, 35);
+            this.btnInfo.TabIndex = 2;
+            this.btnInfo.UseVisualStyleBackColor = true;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackgroundImage = global::WallpaperEVO.Properties.Resources.settings;
+            this.btnSettings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSettings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
+            this.btnSettings.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Location = new System.Drawing.Point(183, 24);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(35, 35);
+            this.btnSettings.TabIndex = 1;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearch.Location = new System.Drawing.Point(753, 28);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(86, 29);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Apply";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogo.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.lblLogo.Location = new System.Drawing.Point(402, 0);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(218, 36);
+            this.lblLogo.TabIndex = 3;
+            this.lblLogo.Text = "WallpaperEVO";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -113,6 +188,9 @@ namespace WallpaperEVO
             this.Name = "frmMain";
             this.Text = "WallpaperEVO";
             this.pnlToolbar.ResumeLayout(false);
+            this.pnlToolbar.PerformLayout();
+            this.pnlSearch.ResumeLayout(false);
+            this.pnlSearch.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -124,6 +202,11 @@ namespace WallpaperEVO
         private System.Windows.Forms.Button btnMinimize;
         private System.Windows.Forms.Panel pnlSearch;
         private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.TextBox txbSearch;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblLogo;
     }
 }
 
