42aa45f6c0cc4bc41a64a7dad06e4dbd7e87b364
JonathanCPP <jonathandale56@gmail.com>
2021-09-27 10:21:58 +0200

    Added function to change wallpaper

--- WallpaperEVO/WallpaperEVO/ImageMenu.cs
+++ WallpaperEVO/WallpaperEVO/ImageMenu.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+using Microsoft.Win32;
 using System.Windows.Forms;
 
 namespace WallpaperEVO
@@ -21,14 +24,20 @@ namespace WallpaperEVO
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         public static extern bool ReleaseCapture();
         #endregion
-        private void DragMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        #region Wallpaper set variables
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        public enum Style : int
         {
-            if (e.Button == MouseButtons.Left)
-            {
-                ReleaseCapture();
-                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
-            }
+            Tiled,
+            Centered,
+            Stretched
         }
+        #endregion
 
         public ImageMenu(string link)
         {
@@ -42,9 +51,47 @@ namespace WallpaperEVO
             Close();
         }
 
-        private void MinimizeApplication(object sender, EventArgs e)
+        private void DragMove(object sender, System.Windows.Forms.MouseEventArgs e)
         {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
 
+        public static void Set(Uri uri, Style style)
+        {
+            System.IO.Stream s = new System.Net.WebClient().OpenRead(uri.ToString());
+
+            System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
         }
     }
 }
+
